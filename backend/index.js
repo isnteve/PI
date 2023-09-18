a67c69deac757baa164b348424e8c1815c16d1c3
isnteve <vieiraamaral2609@gmail.com>
2023-09-18 09:32:57 -0300

    Início do CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express();
-app.use(express.json())
-app.listen(9000, () => console.log("OK"));
+app.use(express.urlencoded({ extend: true }));
+app.use(express.json());
+app.listen(3000, () => console.log("OK"));
 
 const mysql = require('mysql2/promise')
 const connection = mysql.createPool({
@@ -15,30 +16,38 @@ app.get('/', (req, res) => {
     res.send("Evelin");
 })
 
-app.get('/', (req,res)=>{
-res.send("Kauan Barbosa Machado");
+app.get('/', (req, res) => {
+    res.send("Kauan Barbosa Machado");
 })
 
-const getAllPessoas = async () =>{
-const [query] = await connection
-.execute ('select * from TestePessoa.Pessoa')
-return query;
+const getAllPessoas = async () => {
+    const [query] = await connection
+        .execute('select * from TestePessoa.Pessoa')
+    return query;
 }
 
-app.get('/pessoa', async (req,res)=>{
-const consulta = await getAllPessoas()
-return res.status(200).json(consulta)
+app.get('/pessoa', async (req, res) => {
+    const consulta = await getAllPessoas()
+    return res.status(200).json(consulta)
 })
 
-app.get('/pessoa/:id', async (req,res)=>{
-const {id} = req.params;
-const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
-if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '})
-return res.status(200).json(query);
+app.get('/pessoa/:id', async (req, res) => {
+    const { id } = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
+    if (query.lenght === 0) return res.status(400).json({ mensagem: 'Nao encontrado. ' })
+    return res.status(200).json(query);
 })
 
-app.post('/pessoa', async (req,res)=>{
-const {nome, email} = req.body
-const [query] = await connection.execute('insert into TestePessoa.Pessoa (nome,email) values(?,?)', [nome,email])
-return query
+app.post('/pessoa', async (req, res) => {
+    const { nome, email } = req.body
+    const [query] = await connection.execute('insert into TestePessoa.Pessoa (nome,email) values(?,?)', [nome, email])
+    return res.status(200).json(query)
 })
+
+app.get('/pessoa/:nome', async (req, res) => {
+    const { nome } = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where nome = ?', [nome]);
+    if (query.lenght === 0) return res.status(400).json({ mensagem: 'Nao encontrado. ' })
+    return res.status(200).json(query);
+})
+
